53eed11884daf4941c22978433e5282052aea916
Anburaj S <anburaj2203@gmail.com>
2024-06-24 10:53:22 +0530

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import logo from "../images/logo.webp";
 
 const Navbar = () => {
@@ -7,20 +7,20 @@ const Navbar = () => {
     "Mission Statement",
     "Learning Resources",
     "Portfolio",
-    "About Us"
+    "About Us",
   ];
 
   return (
-    <nav className='flex justify-around bg-white p-5 w-[68%] m-auto items-center text-[#131212ed] '>
-      <div className='w-[11%]'>
+    <nav className="flex justify-center gap-3 bg-white p-4  m-auto items-center text-[#131212ed] shadow-2xl">
+      <div className="w-[9%] mr-8">
         <img src={logo} alt="logo" />
       </div>
-      <div className='flex gap-4 text-lg'>
+      <div className="flex gap-4 text-lg">
         {navItems.map((item, index) => (
           <p key={index}>{item}</p>
         ))}
       </div>
-      <button className='p-2 bg-[#1e83cb] text-white shadow-custom transition-transform duration-300 hover:-translate-y-2'>
+      <button className="ml-16 p-2 bg-[#1e83cb] text-white shadow-custom transition-transform duration-300 hover:-translate-y-2">
         CONTACT US
       </button>
     </nav>
